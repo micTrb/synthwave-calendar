9b221e3a9b669068e8bf936b1a28e17b5dadf881
micTrb <aedonem@gmail.com>
2022-11-08 19:35:37 +0100

    some styles more added

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -37,20 +37,20 @@ const Calendar: React.FC<Props> = ({ value = new Date(), onChange }) => {
 
       <div className="mt-2 mx-auto w-full">
 
-        <div className="grid grid-cols-7 border-blue-400 border items-center justify-center text-center">
+        <div className="grid grid-cols-7 border-blue-400 border-t border-b border-green-400 items-center justify-center text-center">
           {weekdays.map((day, index) => (
             <Cell
               key={index}
               className="h-12 flex items-start justify-end cursor-pointer"
             >
-              <p className="m-auto text-xl text-pink-500">{day}</p>
+              <p className="m-auto text-xl text-yellow-500 uppercase">{day}</p>
             </Cell>
           ))}
         </div>
 
         <div className="mt-10 grid grid-cols-7 items-center justify-center text-center">
           {Array.from({ length: prefixDays }).map((_, index) => {
-            return <Cell className="bg-black-400" key={index} />;
+            return <Cell className="" key={index} />;
           })}
 
           {Array.from({ length: numDays }).map((_, index) => {
@@ -58,17 +58,16 @@ const Calendar: React.FC<Props> = ({ value = new Date(), onChange }) => {
             return (
               <Cell
                 key={date}
-                className="text-md bg-black-600 hover:bg-green-50 hover:text-4xl text-pink-500
-                flex items-start justify-end cursor-pointer transition-all duration-700                
-                "
+                className="text-lg border-black-500 bg-black-500 hover:border-green-500 hover:border hover:text-4xl text-pink-500
+                flex items-start justify-end cursor-pointer transition-all duration-400"
               >
-                <p>{date}</p>
+                <p className="my-auto mr-2">{date}</p>
               </Cell>
             );
           })}
 
           {Array.from({ length: suffixDays }).map((_, index) => {
-            return <Cell key={index} className="bg-black-400" />;
+            return <Cell key={index} className="" />;
           })}
         </div>
       </div>
