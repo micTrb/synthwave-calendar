fb7b6205c30dea5a20278a166a962a88e8f91b77
micTrb <aedonem@gmail.com>
2022-11-09 19:19:27 +0100

    synthwave bg added - wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
 
   return (
-    <div className="items-center mt-8 sm:w-11/12 w-full mx-auto">
+    <div className="items-center mt-8 sm:w-11/12 w-full mx-auto z-50">
       <Calendar value={currentDate} onChange={setCurrentDate} />
     </div>
   )
